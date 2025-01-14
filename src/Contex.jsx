be634798b815a424712bc6ce8d8d9c51f2371165
rianhasansiam <rianhasan1971@gmail.com>
2025-01-15 01:49:21 +0600

    setup done

---
+++ src/Contex.jsx
@@ -0,0 +1,25 @@
+import { createContext } from "react";
+
+export const contextData=createContext()
+
+
+const Contex = ({children}) => {
+
+
+  
+  
+  
+      const info={
+       
+        
+      }
+    return (
+      <contextData.Provider value={info}>
+      {children}
+      </contextData.Provider>
+    )
+  }
+  
+
+  
+  export default Contex
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,29 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+
+import {
+  RouterProvider,
+} from "react-router-dom";
+import Root from './Root.jsx';
+import Contex from './Contex.jsx';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+
+
+const queryClient = new QueryClient()
+
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+  <Contex>
+
+
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={Root} />
+    </QueryClientProvider>
+
+  </Contex>
+</StrictMode>,
 )
